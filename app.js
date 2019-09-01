c9a8b503373f3b0f12130ad364ddaef023fc5fbf
bigblobby <tom_dempster@hotmail.co.uk>
2019-09-01 16:02:21 +0100

    change to prototype

--- app.js
+++ app.js
@@ -1,18 +1,13 @@
 var carousel = (function(){
 
-    // ----- Carousel settings ----- //
-    var carouselWrapper = document.querySelector('.carousel--wrapper');
-    var dragStart;
-    var dragEnd;
-
-    var carouselSettings = {
-        target: null,
-        threshold: 50,
-        slidesToShow: 1,
-        slideWidth: function(){
+    function Carousel(settings){
+        this.target = document.querySelector(settings.target);
+        this.threshold = settings.threshold;
+        this.slidesToShow = settings.slidesToShow;
+        this.slideWidth = function(){
             return this.target.firstElementChild.clientWidth;
-        },
-        init: function(){
+        };
+        this.init = function(){
             // Calculate left style
             if(this.slidesToShow === 1){
                 this.target.style.left = -this.slideWidth() + 'px';
@@ -22,60 +17,81 @@ var carousel = (function(){
             carouselWrapper.style.width = (this.slideWidth() * this.slidesToShow) + 'px';
 
             // Add event listeners
-            addListeners();
-        }
-    };
+            this.addListeners();
+        };
+        this.boundOut = mouseOut.bind(this);
+        this.boundMove = mouseMove.bind(this);
+        this.boundUp = mouseUp.bind(this);
+    }
+
+    // ----- Carousel settings ----- //
+    var carouselWrapper = document.querySelector('.carousel--wrapper');
+    var dragStart;
+    var dragEnd;
+
+    // var carouselSettings = {
+    //     target: null,
+    //     threshold: 50,
+    //     slidesToShow: 1,
+    //
+    // };
     // ----- Carousel settings (END)----- //
 
     // ----- Carousel event listeners ----- //
-    function addListeners(){
-        carouselSettings.target.addEventListener('mousedown', function(e){
+    Carousel.prototype.addListeners = function(){
+        var self = this;
+
+        console.log(self);
+
+        this.target.addEventListener('mousedown', function(e){
             dragStart = e.pageX;
 
-            carouselSettings.target.addEventListener('mousemove', mouseMove);
-            carouselSettings.target.addEventListener('mouseup', mouseUp);
-            carouselSettings.target.addEventListener('mouseout', mouseOut);
+            self.target.addEventListener('mousemove', self.boundMove);
+            self.target.addEventListener('mouseup', self.boundUp);
+            self.target.addEventListener('mouseout', self.boundOut);
         });
-    }
+    };
     // ----- Carousel event listeners (END)----- //
 
 
     // ----- Mouse events ----- //
     function mouseOut(){
-        moveSlide(0);
+        this.moveSlide(0);
     }
 
     function mouseMove(e){
         dragEnd = e.pageX;
-        carouselSettings.target.style.transform = "translateX(" + dragPos() + "px)";
+        this.target.style.transform = "translateX(" + dragPos() + "px)";
     }
 
     function mouseUp(e){
         //Right
-        if(dragPos() < -carouselSettings.threshold){
-            moveSlide(-1);
-        } else if(dragPos() > carouselSettings.threshold){
-            moveSlide(1);
+        if(dragPos() < -this.threshold){
+            this.moveSlide(-1);
+        } else if(dragPos() > this.threshold){
+            this.moveSlide(1);
         } else {
-            moveSlide(0);
-
+            this.moveSlide(0);
         }
     }
-    // ----- Mouse events (END)----- //
 
-    function moveSlide(direction){
+    Carousel.prototype.moveSlide = function(direction){
+
+        var self = this;
+
+        console.log(self);
 
         // Move slide
-        carouselSettings.target.style.transform = "translateX(" + (direction * carouselSettings.slideWidth()) + "px)";
+        this.target.style.transform = "translateX(" + (direction * this.slideWidth()) + "px)";
 
-        carouselSettings.target.removeEventListener('mousemove', mouseMove);
-        carouselSettings.target.removeEventListener('mouseup', mouseUp);
-        carouselSettings.target.removeEventListener('mouseout', mouseOut);
+        this.target.removeEventListener('mousemove', self.boundMove);
+        this.target.removeEventListener('mouseup', self.boundUp);
+        this.target.removeEventListener('mouseout', self.boundOut);
 
-        carouselSettings.target.classList.add('transition');
+        this.target.classList.add('transition');
         setTimeout(function(){
-            var firstSlide = carouselSettings.target.firstElementChild;
-            var lastSlide = carouselSettings.target.lastElementChild;
+            var firstSlide = self.target.firstElementChild;
+            var lastSlide = self.target.lastElementChild;
 
             if(direction === 1){
                 firstSlide.insertAdjacentElement('beforebegin', lastSlide);
@@ -83,10 +99,13 @@ var carousel = (function(){
                 lastSlide.insertAdjacentElement('afterend', firstSlide);
             }
 
-            carouselSettings.target.style.transform = "translateX(0px)";
-            carouselSettings.target.classList.remove('transition');
+            self.target.style.transform = "translateX(0px)";
+            self.target.classList.remove('transition');
         }, 400)
-    }
+    };
+    // ----- Mouse events (END)----- //
+
+
 
     function dragPos() {
         return dragEnd - dragStart;
@@ -98,11 +117,9 @@ var carousel = (function(){
             return console.error('Target must be passed into carousel.init()');
         }
 
-        carouselSettings.target = document.querySelector(settings.target);
-        if(settings.threshold) carouselSettings.threshold = settings.threshold;
-        if(settings.slidesToShow) carouselSettings.slidesToShow = settings.slidesToShow;
+        var car = new Carousel(settings);
 
-        carouselSettings.init();
+        car.init();
     }
     // ----- Initialise carousel (END)----- //
 
@@ -116,3 +133,9 @@ carousel.init({
     threshold: 100,
     slidesToShow: 1
 });
+
+// carousel.init({
+//     target: '.carousel-2',
+//     threshold: 100,
+//     slidesToShow: 2
+// });
